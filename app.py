5a4834e484c21ae4ad8adb6e7fe8e93ce7d0370f
thepratiksolanki <pratiks.solanki3@gmail.com>
2025-09-03 20:14:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,5 +137,5 @@ def search():
     return jsonify(final_results)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
